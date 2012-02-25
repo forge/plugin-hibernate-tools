c081b3ac7d1084da881beb1019440fd9459dab59
Koen Aers <koen.aers@gmail.com>
2012-02-25 15:01:28 +0100

    Implemented FORGE-482

--- src/main/java/org/hibernate/forge/plugin/GenerateEntities.java
+++ src/main/java/org/hibernate/forge/plugin/GenerateEntities.java
@@ -1,9 +1,10 @@
 package org.hibernate.forge.plugin;
 
 import java.io.File;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.sql.Driver;
+import java.sql.DriverManager;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
@@ -20,7 +21,6 @@ import org.hibernate.tool.hbm2x.ArtifactCollector;
 import org.hibernate.tool.hbm2x.POJOExporter;
 import org.jboss.forge.project.Project;
 import org.jboss.forge.project.facets.JavaSourceFacet;
-import org.jboss.forge.resources.Resource;
 import org.jboss.forge.shell.PromptType;
 import org.jboss.forge.shell.Shell;
 import org.jboss.forge.shell.ShellColor;
@@ -43,73 +43,68 @@ import org.jboss.shrinkwrap.descriptor.api.spec.jpa.persistence.Property;
 public class GenerateEntities implements Plugin
 {
 
-   private final Shell shell;
-   private final Project project;
-
    @Inject
-   public GenerateEntities(final Project project, final Shell shell)
-   {
-      this.project = project;
-      this.shell = shell;
-   }
+   private Shell shell;
+   
+   @Inject
+   private Project project;
 
    @DefaultCommand(help = "Generate entities from a datasourc.")
    public void newEntity(
-         @Option(name = "table",  
-               help = "Table pattern to include. 'CUSTOMER' for specific table, 'CUST*' for substring match and '*' for all (the default)", 
-               defaultValue = "*")
-         final String tableFilter, 
-         @Option(required = false, name = "schema",
-               help = "Schema pattern to include. Same syntax as for table", 
-               defaultValue = "*")
-         final String schemaFilter, 
-         @Option(required = false, name = "catalog",  
-               help = "Catalog pattern to include. Same syntax as for table", 
-               defaultValue = "*")
-         final String catalogFilter,  
-         @Option(name = "entityPackage", help="Package to use for generated entities.") 
-         String entityPackage,
-         @Option(name = "driver", help = "Class name for JDBC driver", defaultValue = "org.hsqldb.jdbcDriver")
-         final String jdbcDriver,
-         @Option(name = "url", help = "URL for JDBC connection", defaultValue = "jdbc:hsqldb:localhost:9001")
-         final String jdbcURL, 
-         @Option(name = "user", help="Username for JDBC connection", defaultValue = "sa")
-         final String jdbcUsername, 
-         @Option(name = "dialect", help="Dialect to use for database", required = false)
-         String dialect,
-         @Option(name = "password", help="Password for JDBC connection")
-         final String jdbcPassword, 
-         @Option(name = "detectManyToMany", help="Detect many to many associations between tables.", defaultValue = "true")
-         final boolean detectManyToMany, 
-         @Option(name = "detectOneToOne", help="Detect one-to-one associations between tables.", defaultValue = "true")
-         final boolean detectOneToOne, 
-         @Option(name = "detectOptimisticLock", help="Detect optimistic locking tables, i.e. if a table has a column named 'version' with a numeric type optimistic locking will be setup for that table.", defaultValue = "true")
-         final boolean detectOptimisticLock) throws MalformedURLException
-   {
+            @Option(name = "table",
+                     help = "Table pattern to include. 'CUSTOMER' for specific table, 'CUST*' for substring match and '*' for all (the default)",
+                     defaultValue = "*") final String tableFilter,
+            @Option(required = false, name = "schema",
+                     help = "Schema pattern to include. Same syntax as for table",
+                     defaultValue = "*") final String schemaFilter,
+            @Option(required = false, name = "catalog",
+                     help = "Catalog pattern to include. Same syntax as for table",
+                     defaultValue = "*") final String catalogFilter,
+            @Option(name = "entityPackage", help = "Package to use for generated entities.") String entityPackage,
+            @Option(name = "driver", help = "Class name for JDBC driver", defaultValue = "org.hsqldb.jdbcDriver") final String jdbcDriver,
+            @Option(name = "pathToDriverJar", help = "Path in the local file system to the jar file containing the JDBC driver", required = false) final String pathToDriverJar,
+            @Option(name = "url", help = "URL for JDBC connection", defaultValue = "jdbc:hsqldb:localhost:9001") final String jdbcURL,
+            @Option(name = "user", help = "Username for JDBC connection", defaultValue = "sa") final String jdbcUsername,
+            @Option(name = "dialect", help = "Dialect to use for database", required = false) String dialect,
+            @Option(name = "password", help = "Password for JDBC connection") final String jdbcPassword,
+            @Option(name = "detectManyToMany", help = "Detect many to many associations between tables.", defaultValue = "true") final boolean detectManyToMany,
+            @Option(name = "detectOneToOne", help = "Detect one-to-one associations between tables.", defaultValue = "true") final boolean detectOneToOne,
+            @Option(name = "detectOptimisticLock", help = "Detect optimistic locking tables, i.e. if a table has a column named 'version' with a numeric type optimistic locking will be setup for that table.", defaultValue = "true") final boolean detectOptimisticLock) throws Exception 
+      {
+
       PersistenceFacet jpa = project.getFacet(PersistenceFacet.class);
 
-      if(dialect == null) {
-         if(jpa.getConfig().listUnits().size()>0) {
+      if (dialect == null)
+      {
+         if (jpa.getConfig().listUnits().size() > 0)
+         {
             List<Property> properties = jpa.getConfig().listUnits().get(0).getProperties();
-            for (Property property : properties) {
-               if(property.getName().equals("hibernate.dialect")) {
+            for (Property property : properties)
+            {
+               if (property.getName().equals("hibernate.dialect"))
+               {
                   dialect = (String) property.getValue();
                   break;
                }
             }
          }
-         if(dialect==null) {
+         if (dialect == null)
+         {
             shell.println(ShellColor.RED, "Need to specify dialect.");
             return;
          }
-      } 
-      
+      }
+
       JavaSourceFacet java = project.getFacet(JavaSourceFacet.class);
 
       if (entityPackage == null)
       {
-         entityPackage = shell.promptCommon("In which package you'd like to create this @Entity, or enter for default:", PromptType.JAVA_PACKAGE, jpa.getEntityPackage());
+         entityPackage = shell.promptCommon(
+                  "In which package you'd like to create this @Entity, or enter for default:", PromptType.JAVA_PACKAGE,
+                  jpa.getEntityPackage());
       }
+      
+     
 
       JDBCMetaDataConfiguration jmdc = new JDBCMetaDataConfiguration();
 
@@ -118,7 +113,6 @@ public class GenerateEntities implements Plugin
       properties.setProperty("hibernate.connection.driver_class", jdbcDriver);
       properties.setProperty("hibernate.connection.username", jdbcUsername);
 
-      
       properties.setProperty("hibernate.dialect", dialect);
       properties.setProperty("hibernate.connection.password", jdbcPassword == null ? "" : jdbcPassword);
       properties.setProperty("hibernate.connection.url", jdbcURL);
@@ -128,20 +122,54 @@ public class GenerateEntities implements Plugin
       DefaultReverseEngineeringStrategy defaultStrategy = new DefaultReverseEngineeringStrategy();
       ReverseEngineeringStrategy strategy = defaultStrategy;
 
-      ReverseEngineeringSettings revengsettings = new ReverseEngineeringSettings(strategy).setDefaultPackageName(entityPackage).setDetectManyToMany(detectManyToMany).setDetectOneToOne(detectOneToOne).setDetectOptimisticLock(detectOptimisticLock);
+      ReverseEngineeringSettings revengsettings = new ReverseEngineeringSettings(strategy)
+               .setDefaultPackageName(entityPackage).setDetectManyToMany(detectManyToMany)
+               .setDetectOneToOne(detectOneToOne).setDetectOptimisticLock(detectOptimisticLock);
       ;
 
       defaultStrategy.setSettings(revengsettings);
       strategy.setSettings(revengsettings);
       jmdc.setReverseEngineeringStrategy(strategy);
 
-      try {
-         jmdc.readFromJDBC();
-      } catch(HibernateException he) {
-         if(he.getMessage().contains(jdbcDriver)) {
-            shell.println(ShellColor.RED, "Driver class: " + jdbcDriver + " could not be loaded. Check that the driver jar(s) are in $FORGE_HOME/lib.");
+      ClassLoader savedClassLoader = Thread.currentThread().getContextClassLoader();
+
+      try
+      {
+         if (pathToDriverJar != null)
+         {
+            URL url = new File(pathToDriverJar).toURI().toURL();
+            URLClassLoader newlassLoader = new URLClassLoader(new URL[] { url }, savedClassLoader);
+            Thread.currentThread().setContextClassLoader(newlassLoader);
          }
-         throw he;
+         
+         Driver driver = (Driver) Class.forName(jdbcDriver, true, Thread.currentThread().getContextClassLoader())
+                  .newInstance();
+         DriverManager.registerDriver(new DelegatingDriver(driver));
+         DriverManager.getConnection(jdbcURL, jdbcUsername, "").getMetaData();
+
+         try
+         {
+
+            jmdc.readFromJDBC();
+
+         }
+         catch (HibernateException e)
+         {
+            if (e.getMessage().contains(jdbcDriver))
+            {
+               shell.println(ShellColor.RED, "Driver class: " + jdbcDriver
+                        + " could not be loaded. Check that the driver jar(s) are in $FORGE_HOME/lib.");
+            }
+            throw e;
+         }
+      }
+      catch (Exception e) {
+         e.printStackTrace(System.out);
+         throw e;
+      }
+      finally
+      {
+         Thread.currentThread().setContextClassLoader(savedClassLoader);
       }
 
       Iterator<?> iter = jmdc.getTableMappings();
@@ -156,7 +184,7 @@ public class GenerateEntities implements Plugin
 
       POJOExporter pj = new POJOExporter(jmdc, java.getSourceFolder().getUnderlyingResourceObject());
       Properties pojoProperties = new Properties();
-      pojoProperties.setProperty("java5","true");
+      pojoProperties.setProperty("java5", "true");
       pojoProperties.setProperty("ejb3", "true");
       pj.setProperties(pojoProperties);
 
@@ -165,17 +193,19 @@ public class GenerateEntities implements Plugin
          @Override
          public void addFile(final File file, final String type)
          {
-          //  shell.println("Generated " + type + " at " + file.getPath());
+            shell.println("Generated " + type + " at " + file.getPath());
             super.addFile(file, type);
          }
-      };
+      }; 
       pj.setArtifactCollector(artifacts);
       pj.start();
-      Set fileTypes = artifacts.getFileTypes();
-      for (Iterator iterator = fileTypes.iterator(); iterator.hasNext();)
+      Set<?> fileTypes = artifacts.getFileTypes();
+      for (Iterator<?> iterator = fileTypes.iterator(); iterator.hasNext();)
       {
          String type = (String) iterator.next();
          shell.println("Generated " + artifacts.getFileCount(type) + " " + type + " files.");
       }
+      
    }
+
 }
